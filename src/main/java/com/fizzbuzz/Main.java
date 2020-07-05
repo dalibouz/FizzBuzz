7fa9ea9140863562a06da50321bfdbafa2459061
Mohamed Ali <mohamedalibouzrati@gmail.com>
2020-07-05 13:52:51 +0200

    stage 1

--- src/main/java/com/fizzbuzz/Main.java
+++ src/main/java/com/fizzbuzz/Main.java
@@ -3,6 +3,20 @@ package main.java.com.fizzbuzz;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("test");
+        for (int i=1; i<= 100; i++) {
+            if (isDivisableBy(i, 3) && isDivisableBy(i, 5)) {
+                System.out.println("FizzBuzz");
+            } else if (isDivisableBy(i, 3)) {
+                System.out.println("Fizz");
+            } else if (isDivisableBy(i, 5)) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+
+    private static Boolean isDivisableBy(int value, int divisibleBy) {
+        return value % divisibleBy == 0;
     }
 }
