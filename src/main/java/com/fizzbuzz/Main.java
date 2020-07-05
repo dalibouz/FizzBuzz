38c6e9f71c179cbecc48ca2fdfd1568026b2396c
Mohamed Ali <mohamedalibouzrati@gmail.com>
2020-07-05 13:59:03 +0200

    stage 1 improvement

--- src/main/java/com/fizzbuzz/Main.java
+++ src/main/java/com/fizzbuzz/Main.java
@@ -4,16 +4,24 @@ public class Main {
 
     public static void main(String[] args) {
         for (int i=1; i<= 100; i++) {
-            if (isDivisableBy(i, 3) && isDivisableBy(i, 5)) {
+            if (isFizz(i) && isBuzz(i)) {
                 System.out.println("FizzBuzz");
-            } else if (isDivisableBy(i, 3)) {
+            } else if (isFizz(i)) {
                 System.out.println("Fizz");
-            } else if (isDivisableBy(i, 5)) {
+            } else if (isBuzz(i)) {
                 System.out.println("Buzz");
             } else {
                 System.out.println(i);
             }
         }
+    }
+
+    private static Boolean isFizz(int value) {
+        return isDivisableBy(value, 3);
+    }
+
+    private static Boolean isBuzz(int value) {
+        return isDivisableBy(value, 5);
     }
 
     private static Boolean isDivisableBy(int value, int divisibleBy) {
